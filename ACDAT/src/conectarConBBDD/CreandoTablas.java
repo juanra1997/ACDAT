e8e8cf65729914da5e26386b7f9381cd356dee12
juanra1997 <juanrainmortal22@yahoo.com>
2018-10-19 12:42:27 +0200

    +

--- ACDAT/src/conectarConBBDD/CreandoTablas.java
+++ ACDAT/src/conectarConBBDD/CreandoTablas.java
@@ -8,18 +8,21 @@ package conectarConBBDD;
 import com.mysql.jdbc.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 /**
  *
  * @author Juanra
  */
-public class CrearConexion {
+public class CreandoTablas {
 
-    private static final String url="jdbc:mysql://localhost/sys", user="root", password="juanra";//&useSSL=false";
     static private Connection con;
+    static private String tabla1="create table departamentos(dept_no tinyint(2) not null primary key, dnombre varchar(15), loc varchar(15))";
+    static private String datos1="insert into departamentos values(10, 'CONTABILIDAD', 'SEVILLA'),(20, 'INVESTIGACION', 'MADRID'),(30, 'PRODUCCION','BARCELONA'),(40, 'VENTAS', 'BILBAO')";
+    static private Statement sentencia;
     
     public static void main(String[] args) {
-
+        
         try {
 
             //Cargar el driver de mysql
@@ -29,9 +32,9 @@ public class CrearConexion {
             //seleccionar la base de datos llamada 'test'
             //con usuario y contraseña del servidor de MySQL: root y juanra(en mi caso)
             //-----------------------------------------------------------------------------------
-            String connectionUrl = "jdbc:mysql://localhost/sys?user=root&password=juanra&useSSL=false";
+            String connectionUrl = "jdbc:mysql://localhost/empresa?user=root&password=juanra&useSSL=false";
             //-----------------------------------------------------------------------------------
-            
+
             //Obtener la conexion
             //-----------------------------------------------------------------------------------
             /*Connection*/ con = (Connection) DriverManager.getConnection(connectionUrl);
@@ -39,19 +42,44 @@ public class CrearConexion {
             // Connection con=(Connection) DriverManager.getConnection(url, user, password);
             //-----------------------------------------------------------------------------------
             System.out.println("Conexion establecida con exito");
+            //PreparedStatement stmt = con.prepareStatement("create table departamentos(dept_no tinyint(2) not null primary key, dnombre varchar(15), loc varchar(15))");
+            /*stmt.execute();
+            stmt.close();
+            stmt=con.prepareStatement("insert into departamentos values(10, 'CONTABILIDAD', 'SEVILLA'");*/
+            sentencia=con.createStatement();
+            sentencia.execute(tabla1);
+            System.out.println("Tabla creada");
+            sentencia.execute(datos1);
+            System.out.println("Datos introducidos");
         } catch (SQLException e) {
             System.out.println("SQL Exception: " + e.toString());
         } catch (ClassNotFoundException cE) {
             System.out.println("Exception: " + cE.toString());
-        } finally{
-            if(con!=null){
-                try{
+        }catch(Exception e){
+            System.err.println(e);
+        } finally {
+            if (sentencia != null) {
+                try {
+                    sentencia.close();
+                    System.out.println("Sentencia cerrada con exito");
+                } catch (SQLException ex) {
+
+                }
+            }
+            if (con != null) {
+                try {
                     con.close();
                     System.out.println("Conexion cerrada con exito");
-                } catch(SQLException ex){
-                    
+                } catch (SQLException ex) {
+
                 }
             }
         }
     }
+
+    /*public void CrearDept() {
+
+        String tabla = "create table departamentos(dept_no tinyint(2) not null primary key, dnombre varchar(15), loc varchar(15));";
+
+    }*/
 }
---
+++ ACDAT/src/conectarConBBDD/HaciendoConsultas.java
@@ -0,0 +1,95 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package conectarConBBDD;
+
+import com.mysql.jdbc.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+/**
+ *
+ * @author Juanra
+ */
+public class HaciendoConsultas {
+
+    static private Connection con;
+    //static private String tabla1="create table departamentos(dept_no tinyint(2) not null primary key, dnombre varchar(15), loc varchar(15))";
+    //static private String datos1="insert into departamentos values(10, 'CONTABILIDAD', 'SEVILLA'),(20, 'INVESTIGACION', 'MADRID'),(30, 'PRODUCCION','BARCELONA'),(40, 'VENTAS', 'BILBAO')";
+    static private Statement sentencia;
+    static private ResultSet result;
+    static private String consulta = "select * from departamentos";
+
+    public static void main(String[] args) {
+
+        try {
+
+            //Cargar el driver de mysql
+            Class.forName("com.mysql.jdbc.Driver");
+
+            //Cadena de conexion para conectar con MySQL en localhost,
+            //seleccionar la base de datos llamada 'test'
+            //con usuario y contraseña del servidor de MySQL: root y juanra(en mi caso)
+            //-----------------------------------------------------------------------------------
+            String connectionUrl = "jdbc:mysql://localhost/empresa?user=root&password=juanra&useSSL=false";
+            //-----------------------------------------------------------------------------------
+
+            //Obtener la conexion
+            //-----------------------------------------------------------------------------------
+            /*Connection*/ con = (Connection) DriverManager.getConnection(connectionUrl);
+            //-----------------------------------------------------------------------------------
+            // Connection con=(Connection) DriverManager.getConnection(url, user, password);
+            //-----------------------------------------------------------------------------------
+            System.out.println("Conexion establecida con exito");
+            //PreparedStatement stmt = con.prepareStatement("create table departamentos(dept_no tinyint(2) not null primary key, dnombre varchar(15), loc varchar(15))");
+            /*stmt.execute();
+             stmt.close();
+             stmt=con.prepareStatement("insert into departamentos values(10, 'CONTABILIDAD', 'SEVILLA'");*/
+            sentencia = con.createStatement();
+            //sentencia.execute(tabla1);
+            //System.out.println("Tabla creada");
+            //sentencia.execute(datos1);
+            //System.out.println("Datos introducidos");
+            result = sentencia.executeQuery(consulta);
+            while (result.next()) {
+                System.out.println(System.out.printf("%2d %-15s %s\n", result.getInt(1), result.getString("dnombre"), result.getString(3)));  
+            }
+            System.out.println("\nTabla 'departamentos' consultada correctamente");
+        } catch (SQLException e) {
+            System.out.println("SQL Exception: " + e.toString());
+        } catch (ClassNotFoundException cE) {
+            System.out.println("Exception: " + cE.toString());
+        } catch (Exception e) {
+            System.err.println(e);
+        } finally {
+            if (result != null) {
+                try {
+                    result.close();
+                    System.out.println("Sentencia cerrada con exito");
+                } catch (SQLException ex) {
+
+                }
+            }
+            if (sentencia != null) {
+                try {
+                    sentencia.close();
+                    System.out.println("Sentencia cerrada con exito");
+                } catch (SQLException ex) {
+
+                }
+            }
+            if (con != null) {
+                try {
+                    con.close();
+                    System.out.println("Conexion cerrada con exito");
+                } catch (SQLException ex) {
+
+                }
+            }
+        }
+    }
+}
